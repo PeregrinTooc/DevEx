5d80d5753613c3267d747fc0ab6e7f73503afc66
Philipp Rembold <Philipp.Rembold@sap.com>
2020-12-09 15:35:26 +0100

    reformat main

--- main.hs
+++ main.hs
@@ -8,15 +8,15 @@ import TestExercise_3
 import Control.Monad   
 main :: IO ()
 main = do  
-    print ("Enter test to run (1,2,3): ")
+    print "Enter test to run (1,2,3): "
     c <- getChar
     when (c == '1') $ do  
-        print ("Run test for first exercise")
+        print "Run test for first exercise"
         testExercise_1  
     when (c == '2') $ do  
-        print ("Run test for second exercise")
+        print "Run test for second exercise"
         testExercise_2
     when (c == '3') $ do  
-        print ("Run test for third exercise")  
+        print "Run test for third exercise"
         testExercise_3
     main
