6a6aa873636afce23e4af7779ead66e1935e7390
Philipp Rembold <Philipp.Rembold@sap.com>
2020-12-10 11:53:37 +0100

    Solutions

--- exercises/Exercise_2.hs
+++ exercises/Exercise_2.hs
@@ -2,9 +2,15 @@ module Exercise_2 where
 
 --Implement a function „maxElement“ that determines the maximum value of a list of positive numbers
 maxElement:: [Int] -> Int
-maxElement xs = 42
+maxi [a,b] = if a > b then a else b 
+maxElement (x:y:xs) = let a = maxElement (y:xs) in if a > x then a else x 
+maxElement [x] = x
+
+
 
 -- Implement a function „averageList“ that determines the average value of a list of numbers using tail recursion
 -- ATTENTION: For most participants the type definition of this function is confusing. Hence, it is often easier to remove the function definition and use GHCi type inference (but it is necessary for `Exercise_test.hs` script to compile)
 averageList:: (Fractional t, Eq t) => [t] -> t -- remove this line to use GHCi type inference
-averageList xs = 12.0
+averageList xs = average xs 0 0 where 
+    average  [x] count acc = (acc+x)/(count+1)
+    average (x:xs) count acc = average(xs) (count + 1) (acc + x)  
--- exercises/Exercise_3.hs
+++ exercises/Exercise_3.hs
@@ -2,8 +2,10 @@ module Exercise_3 where
   
 --Implement a function “sumList” that adds up all elements of a positive integer list (use Haskell’s “foldl”)
 sumList:: [Int] -> Int
-sumList xs = 42 -- remove parameter xs together with default result and start with "sumListEven = " 
+sumList = foldl (+) 0
       
 --Implement a function “sumListEven” that adds up all even elements of a positive integer list
 sumListEven:: [Int] -> Int
-sumListEven xs = 42 -- remove parameter xs together with default result and start with "sumListEven = " 
+sumListEven = foldl addIfEven 0 where
+    addIfEven:: Int-> Int-> Int
+    addIfEven x y = if  mod y 2 == 0 then x+y else x
